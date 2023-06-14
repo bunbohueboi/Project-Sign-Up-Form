fe368f6ef2b6a48775a165d521c3cccb66e206ec
bunbohueboi <randytn95@gmail.com>
2023-06-13 20:02:02 -0600

    Added row containers to separate fields instead of two columns. Unmatching password message added on password error.

---
+++ script.js
@@ -0,0 +1,26 @@
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+const button = document.querySelector(".create-account");
+
+
+// change password input border to red and print unmatching password message
+button.addEventListener('click', ()=>{
+
+    //will only display 1 instance of 'Passwords do not match'
+    if (password.value != confirmPassword.value && document.querySelector('.unmatching-pass')===null){
+        const unmatchingPass=document.createElement('p');
+        unmatchingPass.classList.add('unmatching-pass');
+        unmatchingPass.textContent='* Passwords do not match'
+        unmatchingPass.style.color='red';
+        unmatchingPass.style.margin='0px';
+        const passwordContainer=document.querySelectorAll('.field-container')[4];
+        passwordContainer.append(unmatchingPass);
+
+
+        password.style.boxShadow= "0px 0px 1.5px red";
+        password.style.borderColor='red';
+        confirmPassword.style.boxShadow='0px 0px 1.5px red';     
+        confirmPassword.style.borderColor='red';                                                                             
+    }
+});
+
